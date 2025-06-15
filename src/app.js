a2cef2677f89fae91437ce1f5ec245aec1afc35f
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-06-15 21:51:10 +0530

    add routes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connect from "./db/index.js";
+import cookieParser from "cookie-parser";
+
 import greetRoute from "./routes/greet.routes.js"
 import userRouter from "./routes/user.routes.js"
-import cookieParser from "cookie-parser";
+import commentRouter from "./routes/comment.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import videoRouter from "./routes/video.routes.js"
 
 const app = express();
 dotenv.config();
@@ -22,5 +30,12 @@ app.use(
 
 app.use("/api/v1/greet",greetRoute)
 app.use("/api/v1/users",userRouter)
+app.use("/api/v1/comments",commentRouter)
+app.use("/api/v1/dashboard",dashboardRouter)
+app.use("/api/v1/likes",likeRouter)
+app.use("/api/v1/playlists",playlistRouter)
+app.use("/api/v1/subscriptions",subscriptionRouter)
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/videos",videoRouter)
 
 export default app;
